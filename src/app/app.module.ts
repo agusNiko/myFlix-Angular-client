d9b586eac1b83d3a9a9082433ed0ed1c6097e888
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-07-29 09:34:18 +0200

    add HttpClientModule
    HttpClientModule makes it possible for the client app to communicate with the API or server-side.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//HttpClientModule is a simplified API for Angular applications that makes it possible for the client app to communicate with the API or server-side.
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
