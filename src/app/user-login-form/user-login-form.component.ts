4c2e9cf20cc79e26fc3958b15fef1cab8e84574e
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-08-07 18:07:06 +0200

    creates the logic for  UserLoginFormComponent
    fetch userLogin() from fetchApiData so the http request can be done

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input } from '@angular/core';
+// this is used to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+// This brings in the API calls 
+import { ApiDataService } from '../fetch-api-data.service';
+// this is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: ApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+
+  // This is the function responsible for sending the form inputs to the backend
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData.Username, this.userData.Password).subscribe((result) => {
+      // Logic for a successful user registration goes here! (To be implemented)
+      console.log('user accessed to myFlix')
+      this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+}
