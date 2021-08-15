e0ef6e311b020adbf137d024089bebec665b8935
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-08-15 10:44:53 +0200

    moving buttons to welcome page
    moved the button and log in and sing up logic to welcome-page component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+// import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+// import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
-
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +13,8 @@ export class AppComponent {
   title = 'myFlix-Angular-client';
 
   constructor(public dialog: MatDialog) { }
-  // This is the function that will open the dialog when the signup button is clicked  
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '280px'
-    });
-  }
-  openLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, { width: '280px' })
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, { width: '500px' });
   }
 }
