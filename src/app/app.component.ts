e73959da2b345ef8875b5688c4d78256151e9ef4
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-09-09 10:51:30 +0200

    adding logout button and profile button to the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,9 +14,25 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 export class AppComponent {
   title = 'myFlix-Angular-client';
 
-  constructor(public dialog: MatDialog, private route: ActivatedRoute) { }
+  constructor(
+    public dialog: MatDialog,
+    private route: ActivatedRoute,
+    public Router: Router) { }
 
   openMoviesDialog(): void {
     this.dialog.open(MovieCardComponent, { width: '500px' });
   }
+
+  goToProfile(): void {
+    this.Router.navigate(['profile']);
+  }
+
+  logOut(): void {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    this.Router.navigate(['welcome']);
+
+
+  }
+
 }
