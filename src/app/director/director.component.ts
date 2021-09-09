ff58e85a63383fd74f774aa7089ed6084eeddb29
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-09-09 10:52:30 +0200

    Implemented getDirector to the component and updated the template

--- src/app/director/director.component.ts
+++ src/app/director/director.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiDataService } from '../fetch-api-data.service'
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-director',
@@ -8,11 +11,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DirectorComponent implements OnInit {
   name: any = '';
-  constructor(private route: ActivatedRoute) { }
+  director: any = '';
+
+
+  constructor(
+    private route: ActivatedRoute,
+    public fetchApiData: ApiDataService,
+    public Router: Router,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.name = this.route.snapshot.paramMap.get("name")
-    console.log(this.name)
+    this.name = this.route.snapshot.paramMap.get("name");
+    console.log(this.name);
+    this.getDirector(this.name);
   }
 
+  getDirector(id: any): void {
+    this.fetchApiData.getDirector(id).subscribe((resp: any) => {
+      this.director = resp;
+      console.log(this.director.Name);
+      return this.director;
+    });
+  }
+
+  goBack(): void {
+    this.Router.navigate(['movies']);
+  }
 }
